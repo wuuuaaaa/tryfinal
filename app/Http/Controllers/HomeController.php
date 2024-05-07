2982aa27d505fcc1f8702ba07fd24b6c27c43e1e
Teoh Kiat Koon <kiatkoon@yahoo.com>
2024-05-07 16:42:21 +0800

    new function at Home

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -9,4 +9,9 @@ class HomeController extends Controller
     public function index() {
         // new function introduce
     }
+
+    public function dashboard() {
+        // function from origin
+    }
+}
 }
